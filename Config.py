c4befe2d134d13170b31d7a8f96c85264dc4dcb7
Kat9274 <Kat9274@icloud.com>
2019-11-17 09:46:02 -0600

    Version 0.1.0 Clean Code

--- Config.py
+++ Config.py
@@ -1,52 +1,58 @@
-ObjectsName = []
-
 class Object:
     def __init__(self, Name, Look, Use, Take, Leave, On_Use, Args, On):
-        #got rid of pos in favor of items lists
-        self.Name = Name #String
-        self.Look = Look #String
-        self.Use = Use #List
-        self.Take = Take #String
-        self.Leave = Leave #String
+        global ObjectsName, Object_Table
+        self.Name = Name  # String
+        self.Look = Look  # String
+        self.Use = Use  # List
+        self.Take = Take  # String
+        self.Leave = Leave  # String
 
-        self.On_Use = On_Use #Function
-        self.Args = Args #Tuple
-        self.On = On #Bool
+        self.On_Use = On_Use  # Function
+        self.Args = Args  # Tuple
+        self.On = On  # Bool
 
         ObjectsName.append(Name.lower())
         Object_Table[str(Name).lower()] = self
 
+
 class Player:
     def __init__(self):
-        self.Pos = [0, 0] #List
-        self.Items = [] #List
+        global ObjectsName
+        self.Pos = [0, 0]  # List
+        self.Items = []  # List
 
         ObjectsName.append("i")
 
-#Needed:
-Player = Player() #Define "Player"
-RoomList = [] #Define "RoomList"
-Object_Table = {"i": Player}
-In_Prompt = ">>> "
 
 class Room:
-   def __init__(self, Pos, Look):
-       global RoomList
-       self.Pos = Pos #List
-       self.Items = [self] #List
-       self.Look = Look #String
-       RoomList.append(self)
+    def __init__(self, Pos, Look):
+        global RoomList
+        self.Pos = Pos  # List
+        self.Items = [self]  # List
+        self.Look = Look  # String
+        RoomList.append(self)
 
-#Rooms:
+
+# Needed:
+ObjectsName = []
+RoomList = []  # Define "RoomList"
+In_Prompt = ">>> "
+Player = Player()  # Define "Player"
+Object_Table = {"i": Player}
+
+# Rooms:
 Room00 = Room([0, 0], "A pretty plain room. There is a fan here. The wall to the north seems strange.")
 
-#Funcs:
+# Funcs:
+
+
 def Fan_Blows_Room00():
     Fan.Use[0] = "The fan turns on"
     Room01 = Room([0, 1], "")
     Room00.Look = "A pretty plain room. There is a fan here. The wall to the north has a door."
     RoomList.append(Room10)
 
-#Objects:
-Fan = Object("Fan", "It's a old fan.", ["The fan turns on. The wind from the fan blows a door to the north open.", "The fan turns off."], False, False, Fan_Blows_Room00, (False, ), False)
+
+# Objects:
+Fan = Object("Fan", "It's a old fan.", ["The fan turns on. The wind from the fan blows a door to the north open.", "The fan turns off."], False, False, Fan_Blows_Room00, (False, False), False)
 Room00.Items.append(Fan)
--- TextGame.py
+++ TextGame.py
@@ -1,11 +1,12 @@
 from Config import *
 
+
 def Parse(Input):
     Input = Input.lower().split()
 
     i = 0
-    Start = "No Value" #Set Start to "No Value" so that if there is no start word it will know
-    while i < len(Input): #See where the first function word is in the list "Input"
+    Start = "No Value"  # Set Start to "No Value" so that if there is no start word it will know
+    while i < len(Input):  # See where the first function word is in the list "Input"
         if Input[i] in Commands:
             Start = i
             break
@@ -13,21 +14,21 @@ def Parse(Input):
             i = i + 1
 
     try:
-        if Start == "No Value": #If there's no command
+        if Start == "No Value":  # If there's no command
             print("What do you want to do?")
             pass
 
         elif Input[Start] in ["stop", "end", "exit", "done", "leave"]:
             exit()
 
-        elif Input[Start] not in ["go"]: #If the command is not "go"
-            try:#Needs the try so when IndexError is raised it will look at the room
+        elif Input[Start] not in ["go"]:  # If the command is not "go"
+            try:  # Needs the try so when IndexError is raised it will look at the room
                 i = 0
                 while Input[(Start + i)] not in ObjectsName:
                     i = i + 1
                     pass
                 Offset = i
-                Command_Table[Input[Start]](Object_Table[Input[(Start + Offset)]]) #Run the command
+                Command_Table[Input[Start]](Object_Table[Input[(Start + Offset)]])  # Run the command
             except IndexError:
                 Command_Table[Input[Start]](FindRoom(Player.Pos))
         elif Input[Start] in ["go"]:
@@ -36,9 +37,9 @@ def Parse(Input):
                 i = i + 1
                 pass
             Offset = i
-            Command_Table[Input[Start]](Input[(Start + Offset)]) #Run the command
+            Command_Table[Input[Start]](Input[(Start + Offset)])  # Run the command
         pass
-    except Exception as e: #Errors
+    except Exception as e:  # Errors
         e = e.__class__.__name__
         if e == "IndexError":
             print("What do you want to do?")
@@ -49,26 +50,31 @@ def Parse(Input):
 def FindRoom(Pos):
     i = 0
     while i < len(RoomList):
-        if RoomList[i].Pos == Pos: #Where room pos == "Pos"
+        if RoomList[i].Pos == Pos:  # Where room pos == "Pos"
             return RoomList[i]
         else:
             i = i + 1
             pass
 
+
 def Look(Object):
     try:
-        print(Object.Look if Object in FindRoom(Player.Pos).Items else f"{Object.Name} isn't here!") #Print the look text or say the object isnt here
+        # Print the look text or say the object isnt here
+        print(Object.Look if Object in FindRoom(Player.Pos).Items else f"{Object.Name} isn't here!")
         return 0
     except:
         return 1
 
+
 def Use(Object):
     try:
-        if Object in FindRoom(Player.Pos).Items: #If the object is in the rooms items list
-            print(Object.Use[int(Object.On)]) #Print the Use text most appropriate
-            Object.On = not Object.On #Toggle on
+        # If the object is in the rooms items list
+        if Object in FindRoom(Player.Pos).Items:
+            # Print the Use text most appropriate
+            print(Object.Use[int(Object.On)])
+            Object.On = not Object.On  # Toggle on
             if Object.On_Use != False:
-                if Object.Args != (False, ):
+                if Object.Args != (False, False):
                     Object.On_Use(Object.Args)
                 else:
                     Object.On_Use()
@@ -78,10 +84,11 @@ def Use(Object):
     except:
         return 1
 
+
 def Take(Object):
     try:
         if Object in FindRoom(Player.Pos).Items:
-            if type(Object.Take).__name__ == "str": #If the object is takeable
+            if type(Object.Take).__name__ == "str":  # If the object is takeable
                 print(Object.Take)
                 Player.Items.append(Object)
                 FindRoom(Player.Pos).Items.pop(FindRooms.Items.index(Object))
@@ -93,20 +100,22 @@ def Take(Object):
     except:
         return 1
 
+
 def Leave(Object):
     try:
         if Object in Player.Items:
-            print(Object.Leave) #Just do the opposite of take
+            print(Object.Leave)  # Just do the opposite of take
             FindRoom(Player.Pos).Items.append(Object)
             Player.Items.Pop(Player.Items.index(Object))
             return 0
     except:
         return 1
 
+
 def WhereIs(Object):
     try:
-        if type(Object).__name__ == "Player": #If player
-            print(f"{Object.Pos[0]}, {Object.Pos[1]}") #Find player
+        if type(Object).__name__ == "Player":  # If player
+            print(f"{Object.Pos[0]}, {Object.Pos[1]}")  # Find player
             return Object.Pos
         else:
             print(f"{RoomList.Items.index(Object).Pos[0]}, {RoomList.Items.index(Object).Pos[1]}")
@@ -114,18 +123,21 @@ def WhereIs(Object):
     except:
         return 1
 
+
 def Go(Direction):
     try:
-        Direction_Table = {"west": [0, -1], "east": [0, 1], "south": [1, -1], "north": [1, 1]} #Table
+        Direction_Table = {"west": [0, -1], "east": [0, 1], "south": [1, -1], "north": [1, 1]}  # Table
         Player.Pos[Direction_Table[Direction][0]] = Player.Pos[Direction_Table[Direction][0]] + Direction_Table[Direction][1]
         if str(type(FindRoom(Player.Pos)).__name__) == "Room":
-            print(FindRoom(Player.Pos).Look) #If the room exists print
+            print(FindRoom(Player.Pos).Look)  # If the room exists print
         else:
-            print("You cant go there!") #If it doesnt exist print and reset the pos
+            # If it doesnt exist print and reset the pos
+            print("You cant go there!")
             Player.Pos[Direction_Table[Direction][0]] = Player.Pos[Direction_Table[Direction][0]] - Direction_Table[Direction][1]
         return 0
     except:
         return 1
+
 
 Commands = ["go", "look", "use", "take", "leave", "where", "stop", "end", "exit", "done", "leave"]
 Command_Table = {"go": Go, "look": Look, "use": Use, "take": Take, "leave": Leave, "where": WhereIs}
