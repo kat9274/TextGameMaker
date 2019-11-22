17c251b212e1d1cdd388e5f0591203ae97fe8af3
Kat9274 <Kat9274@icloud.com>
2019-11-22 08:46:56 -0600

    Version 0.1.1

--- Engine.py
+++ Engine.py
@@ -5,7 +5,8 @@ class Object:
         self.Func_List = kwargs.get("Func_List", [None, [None, None], None, None])
         self.Args_List = kwargs.get("Args_List", [None, [None, None], None, None])
 
-        Room.Item_Table[Name] = self
+        Room.Item_Table[Name.lower()] = self
+        All_Names.append(Name.lower())
 
 class Room:
     def __init__(self, Pos, Look, *args, **kwargs):
@@ -22,35 +23,53 @@ class Player:
 #Look east
 #Look (AT ROOM)
 
-def Parse(Room, Command_Table, All_Objects, *args, **kwargs):
+def Parse(Room, Command_Table, *args, **kwargs):
 
-    #Command_Table needs to be customizable to add new ones
-    #All_Objects Rename to something that implies names of objects
+    #Command_Table needs to be customizable to add new ones #Just do that in the input to the parse func (def Parse(HERE))
 
-    Input = input(kwargs.get("Input_Prompt", ">>> ")).lower().split()
+    Input = input(kwargs.get("Input_Prompt", ">>> ")).lower().split() #Get input and split it up
 
     Start = None
     i = 0
-    while i < len(Input):
+    while i < len(Input): #Find Command and where it is in the list
         if Input[i] in Command_Table:
             Start = i
             break
         i = i + 1
 
-    if Start.__class__.__name__ == "NoneType":
-        print(kwargs.get("No_Command", "What do you want to do?"))
+    if Start.__class__.__name__ == "NoneType": #If there is no command
+        print(kwargs.get("No_Command", "What do you want to do?")) #Print No_Command
 
-    elif Input[Start] in ["stop", "end", "exit", "done"]:
-        print(kwargs.get("Exit_Text", ""))
-        exit()
+    elif Input[Start] in ["stop", "end", "exit", "done"]: #If the command is an exit command
+        print(kwargs.get("Exit_Text", "")) #Print Exit_Text
+        exit() #Exit
 
     Offset = 0
-    while Input[(Start + Offset)] not in All_Objects.extend(["north", "south", "east", "west"]):
+    while Input[(Start + Offset)] not in All_Names.extend(["north", "south", "east", "west"]): #Find Offset to Start that leads to object or direction
         Offset = Offset + 1
 
-    if Room.Object_Table.get(Input[(Start + Offset)], None).__class__.__name__ != "NoneType":
-        Command_Table[Input[Start]](Room.Object_Table.get(Input[(Start + Offset)], None))
-    elif Input[(Start + Offset)] in ["north", "south", "east", "west"]:
-        Command_Table[Input[Start]](Input[(Start + Offset)])
-    else:
-        print(kwargs.get("No_Args", "What do you want to do?"))
+    if Room.Object_Table.get(Input[(Start + Offset)], None).__class__.__name__ != "NoneType": #If the object exists
+        Command_Table[Input[Start]](Room.Object_Table.get(Input[(Start + Offset)], None)) #Run the command with the object
+
+    elif Input[(Start + Offset)] in ["north", "south", "east", "west"]: #If its a direction
+        Command_Table[Input[Start]](Input[(Start + Offset)]) #Run the command with the direction
+
+    else: #If it's not either of those
+        print(kwargs.get("No_Args", "What do you want to do?")) #Print No_Args
+
+a = Room([0, 0], "", Item_Table={"Cat": 1, "Dog": 2, "fan": 3, "a nother cat": 8574, "mom telling me to trim my nails": 4, "light": 5, "Something": 6})
+
+def Dynamic(Room):
+    List = list(Room.Item_Table.keys())
+    Temp = []
+    i = 0
+    while i <= (len(List) - 2):
+        if i == 0:
+            Temp.append(f"There is a {List[i]}")
+        else:
+            Temp.append(f", a {List[i]}")
+        i = i + 1
+    Temp.append(f", and a {List[i]} here.")
+    return ''.join(Temp)
+
+All_Names = []
