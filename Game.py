af18f5a7baa940d1a71b25fc20d070daf13f3055
Kat9274 <Kat9274@icloud.com>
2019-12-02 10:13:45 -0600

    Version 0.1.2

--- Game.py
+++ Game.py
@@ -1,8 +1,9 @@
 from Engine import *
 
 def Win():
-    print("You win!")
-    exit()
+    return 0
+    #print("You win!")
+    #exit()
 
 def Fan1On():
     print("The fan blows down the wall to the north!")
@@ -11,7 +12,7 @@ def Fan1On():
 
 Room00 = Room([0, 0], "It is a pretty bright room.")
 
-Fan1 = Object(Room00, "Fan", Text_List=["It's a fan.", ["You turn the fan on.", "You turn the fan off."], None, None], Func_List=[None, [Win, None], None, None])
+Fan1 = Object(Room00, "Fan", Text_List=["It's a fan.", ["You turn the fan on.", "You turn the fan off."], None, None], Func_List=[Win, [Win, None], None, None])
 
 while True:
     Parse(Input_Prompt=">>> ", Exit_Text="Bye.", No_Command="What?", No_Text="You can't do that!")
